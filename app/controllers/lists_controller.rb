89dad56470c30ddc7792ae07a3cbb2d86ae46af3
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-07-11 22:14:56 +0000

    新規投稿機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @book = Booker.new
+  end
+  
+  def create
+    book = Booker.new(booker_params)
+    book.save
+    redirect_to '/lists'
   end
 
   def index
@@ -9,5 +16,9 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  def booker_params
+    params.require(:booker).permit(:title, :body)
   end
 end
