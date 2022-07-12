e221438ba4b9e10ef3ef81e12824dd9bd640ffd6
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-07-12 11:37:02 +0000

    index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,23 +2,28 @@ class ListsController < ApplicationController
   def new
     @book = Booker.new
   end
-  
+
   def create
-    book = Booker.new(booker_params)
+    book = Booker.new(book_params)
     book.save
-    redirect_to '/lists'
+    redirect_to lists_path(book.id)
   end
 
   def index
+    @books = Booker.all
   end
 
   def show
+    @book = Booker.find(params[:id])
   end
 
   def edit
   end
-  
-  def booker_params
+
+  def book_params
     params.require(:booker).permit(:title, :body)
   end
+  
 end
+
+
--- db/migrate/20220710214030_create_bookers.rb
+++ db/migrate/20220710214030_create_bookers.rb
@@ -1,8 +1,8 @@
 class CreateBookers < ActiveRecord::Migration[6.1]
   def change
     create_table :bookers do |t|
-      t.string :本のタイトル
-      t.string :感想
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
