4e5365f713f911c8211114b1a58971c8dd0dcb36
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-1.compute.internal>
2022-07-14 00:56:38 +0000

    destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,46 @@
 class BooksController < ApplicationController
   def top
   end
+    def new
+    @book = Booker.new
+    @books = Booker.all
+  end
+
+  def create
+    book = Booker.new(book_params)
+    book.save
+    redirect_to "/books/new"
+  end
+
+  def index
+    @books = Booker.all
+  end
+
+  def show
+    @book = Booker.find(params[:id])
+  end
+
+  def edit
+    @book = Booker.find(params[:id])
+  end
+
+  def update
+    book = Booker.find(params[:id])
+    book.update(book_params)
+    redirect_to new_book_path
+  end
+
+  def destroy
+    book = Booker.find(params[:id])
+    book.destroy
+    redirect_to new_book_path
+  end
+
+  private
+  def book_params
+    params.require(:booker).permit(:title, :body)
+  end
+
 end
+
+
